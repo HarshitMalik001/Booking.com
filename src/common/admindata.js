77a47adf932b3466b3f961aaf98c2b19416c5b9b
Harshit Malik <harshit1638.be22@chitkara.edu.in>
2024-05-24 13:58:31 +0530

    first commit

---
+++ src/common/admindata.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+const admindata = [{name:"harshit",mail:"harshit@mail",pass:"12345"},{name:"Harshit",mail:"harshit@mail",pass:"12345"}];
+
+
+export default admindata;
